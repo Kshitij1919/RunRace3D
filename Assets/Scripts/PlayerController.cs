c28f4580334609e73e6886d8a71619ef08bebace
Kshitij9921 <kshitijcobra@gmail.com>
2022-02-21 09:51:33 +0530

    CharacterController
    fixed charactercontroller BUG!!
    
    jumpfunction...

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,116 +1,68 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
-    private float gravityValue = -9.81f;
-
-    private void Start()
-    {
-        //controller = gameObject.AddComponent<CharacterController>();
-        controller = GetComponent<CharacterController>();   
-    }
-
-    void Update()
-    {
-
-        if (controller.isGrounded)
-        {
-            print("CharacterController is grounded");
-        }
-
-
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            print("ground");
-            playerVelocity.y = 0f;
-        }
-
-        //playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-
-
-
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //if (move != Vector3.zero)
-        //{
-        //    gameObject.transform.forward = move;
-        //}
-
-        //// Changes the height position of the player..
-        //if (Input.GetButtonDown("Jump") && groundedPlayer)
-        //{
-        //    print("jump");
-        //    playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        //}
-
-        //playerVelocity.y += gravityValue * Time.deltaTime;
-        //controller.Move(playerVelocity * Time.deltaTime);
-    }
-
-    
-    /*
     CharacterController characterController;
-    private Vector3 motion;
-    public float speed;
-    public float jumpSpeed;
-    private Vector3 jumpVelocity;
-    private bool  isGrounded;
-    public float gravity;
 
-    // Start is called before the first frame update
+    Vector3 playerMove;
+    [SerializeField] 
+    private  float speed;//playerSpeed
+    public float playerJumpForce;
+    public float playerVelocity = 0;
+    public float gravity;
 
     private void Awake()
     {
-        characterController = GetComponent<CharacterController>();
-    }
-    void Start()
-    {
-
+            characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
+            
+    }
 
-        //motion = Vector3.zero;
+     void Update()
+    {
+        playerMove = Vector3.zero;
+        playerMove = transform.forward;
 
-        //motion = transform.forward; //goes forward in player z direction
-        isGrounded = characterController.isGrounded;
-        if (isGrounded && jumpVelocity.y < 0f)
+        if (characterController.isGrounded)
         {
-            //jump logic
-            print("grounded");
-
-            jumpVelocity.y = 0; 
-
-            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
-            {
-                print("Jump");
-                //jumpVelocity.y = jumpSpeed;
-
-            }
+            playerVelocity = 0f;
+            jump();
         }
         else
         {
-            //jumpVelocity = jumpVelocity - gravity * Time.deltaTime;
+            gravity = 30f;
+            playerVelocity -= gravity * Time.deltaTime;
         }
 
 
-        //motion.Normalize();
+        playerMove.Normalize();
 
-        //motion = motion * speed * Time.deltaTime;
-        //characterController.Move(motion);
+        playerMove *= speed;
+        playerMove.y = playerVelocity;
+
+        characterController.Move(playerMove * Time.deltaTime);
+    }
+
+    private void jump()
+    {
 
 
-    } */
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            print("Jump!");
+            playerVelocity = playerJumpForce;
+        }
+        
+
+
+        
+    }
 }
